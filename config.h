da487101bdb9cbd0c7cf91ee037acb389a8518fa
ar-bebop <aribeir4@gmail.com>
2023-09-16 17:07:15 -0400

    changed blocks path. again

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* macro for conveniently specifying pathu and pathc below */
-#define PATH(name)                      "/home/bebop/dots/suckless/dwmblocks/blocks/"name
+#define PATH(name)                      "/home/bebop/suckless/dwmblocks/blocks/"name
 
 /* buffer size for capturing output of the programs used for updating blocks */
 #define CMDOUTLENGTH                    50
