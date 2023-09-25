332af8d96fdd118f92eb33b44135c91cdb63d786
ar-bebop <aribeir4@gmail.com>
2023-09-25 02:49:23 -0400

    moon block

--- blocks/moon.sh
+++ blocks/moon.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-printf "🌑"
+PHASE=$(curl -s "https://www.moongiant.com/phase/today" | grep -oP "Phase: <span>\K.*?(?=<)")
+
+case "$PHASE" in
+    "New Moon")
+        printf "🌕";;
+    "Waxing Crescent")
+        printf "🌖";;
+    "Firt Quarter")
+        printf "🌗";;
+    "Waxing Gibbous")
+        printf "🌘";;
+    "Full Moon")
+        printf "🌑";;
+    "Waning Gibbous")
+        printf "🌒";;
+    "Last Quarter")
+        printf "🌓";;
+    "Waning Crescent")
+        printf "🌔";;
+esac
